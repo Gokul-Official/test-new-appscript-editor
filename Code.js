c8d11fb7139babab092ca6353cf4429be17adaef
Gokul Nagesh <gokul@jivrus.com>
2022-08-05 17:15:32 +0530

    test 2

--- Code.js
+++ Code.js
@@ -6,6 +6,9 @@ function myNewFunction() {
   //New Test Enivroment Cool Cool.
 }
 
-// this is a new function to all out there
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  this
+} //is a new function to all out there
 
 //New Tender comment
