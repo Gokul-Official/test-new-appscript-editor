fdbb3eb4b3298d8f7056ffe6ae07131449b4c2ef
Gokul-Personal <giogokul13@gmail.com>
2022-08-05 16:48:16 +0530

    initial commit

---
+++ Code.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  console.log('This Log is from my Jivrus Account 123456786876');
+}
+
+function myNewFunction() {
+  //New Test Enivroment Cool Cool.
+}
+
+// this is a new function to all out there
