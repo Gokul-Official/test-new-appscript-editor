7e8c02bfe86054d3f227394cf5ab070e5fc9b1f2
Gokul Nagesh <gokul@jivrus.com>
2022-08-05 17:21:38 +0530

    new commit

--- Code.js
+++ Code.js
@@ -8,7 +8,6 @@ function myNewFunction() {
 
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
-  this
 } //is a new function to all out there
 
 //New Tender comment
